c077db44d2727ea22b11c07ef6b842e874adb88d
henrikmotzkus <henrik@motzkus.info>
2025-04-29 14:45:11 +0200

    test button

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Hello World! I'm using Flask. Henrik is the best!"
+    if request.method == "POST":
+        user_input = request.form.get("user_input")
+        return render_template_string('''
+            <!doctype html>
+            <title>Flask Input</title>
+            <h1>You entered: {{ user_input }}</h1>
+            <form method="post">
+                <input type="text" name="user_input" placeholder="Enter something">
+                <button type="submit">Submit</button>
+            </form>
+        ''', user_input=user_input)
+    return render_template_string('''
+        <!doctype html>
+        <title>Flask Input</title>
+        <h1>Enter something below:</h1>
+        <form method="post">
+            <input type="text" name="user_input" placeholder="Enter something">
+            <button type="submit">Submit</button>
+        </form>
+    ''')
+
+if __name__ == "__main__":
+    app.run(debug=True)
